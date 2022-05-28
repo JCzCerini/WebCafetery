14263a49f54244a7f3f48431d4056e3350c4fb80
jczcerini <juancruzcerini@hotmail.com>
2022-05-28 16:08:22 -0300

    Ingreso de proyecto en GitHub

---
+++ js/mainCafe.js
@@ -0,0 +1,6 @@
+let menuBurger = document.querySelector('.menu-burger');
+let navMenu = document.querySelector('.nav-menu');
+
+menuBurger.addEventListener('click', () =>{
+    navMenu.classList.toggle
+})

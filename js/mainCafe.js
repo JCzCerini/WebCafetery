0cf335fc947380631d1321f0bb8f7cedd5834b50
jczcerini <juancruzcerini@hotmail.com>
2022-07-26 08:32:39 -0300

    Restructuracion del codigo y correcciones de la mayor parte

--- js/mainCafe.js
+++
@@ -1,6 +0,0 @@
-let menuBurger = document.querySelector('.menu-burger');
-let navMenu = document.querySelector('.nav-menu');
-
-menuBurger.addEventListener('click', () =>{
-    navMenu.classList.toggle
-})
